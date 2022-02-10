4023d2e4e1f45dc2d2272bb8d02d9f4c6b7583e8
JuDas69 <j.bohorquezb@uniandes.edu.co>
2022-02-10 12:20:47 -0500

    quitar print #3

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    print("3- Cargar Book-Tags!!!...")
+    # TODO: Modificación de Est-1 en el Lab 2
     print("0- Salir")
 
 
